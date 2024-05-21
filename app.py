e12e33e457cf2c45484c18bf417f2cfde0843feb
J. S. Lin <chunhsien.lin@gmail.com>
2024-05-21 23:07:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask, request, abort
-from linebot.v3 import LineBotApi
-from linebot.v3.webhook import WebhookHandler
-from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
 from linebot.models import *
 import os
 import requests
