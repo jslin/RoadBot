1bca3b3711e2cfd2ada59c3a3d7d9013d5ce2b0e
J. S. Lin <chunhsien.lin@gmail.com>
2024-05-22 20:42:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, abort
-#from linebot import LineBotApi
-# from linebot.v3.messaging import MessagingApi
-from linebot.v3.webhook import WebhookHandler
+from linebot import LineBotApi
+from linebot.webhook import WebhookHandler
 from linebot.v3.exceptions import InvalidSignatureError
 from linebot.v3.messaging import (
     Configuration,
@@ -10,10 +9,11 @@ from linebot.v3.messaging import (
     ReplyMessageRequest,
     TextMessage
 )
-from linebot.v3.webhooks import (
-    MessageEvent,
-    TextMessageContent
-)
+#from linebot.v3.webhooks import (
+#    WebhookHandler,
+#    MessageEvent,
+#    TextMessageContent
+#)
 from linebot.models import *
 import os
 import requests
@@ -22,7 +22,7 @@ import json
 app = Flask(__name__)
 
 configuration = Configuration(access_token=os.environ['CHANNEL_ACCESS_TOKEN'])
-#line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
+line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 llm_server_url = os.environ['REMOTE_LLM_SERVER']
 
@@ -66,26 +66,26 @@ def callback():
         abort(400)
     return 'OK'
 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    prompt = event.message.text
+    llm_text = llm_responser(llm_server_url, prompt)
+    message = TextSendMessage(text=llm_text)
+    line_bot_api.reply_message(event.reply_token, message)
+
 #@handler.add(MessageEvent, message=TextMessage)
 #def handle_message(event):
 #    prompt = event.message.text
 #    llm_text = llm_responser(llm_server_url, prompt)
-#    message = TextSendMessage(text=llm_text)
-#    line_bot_api.reply_message(event.reply_token, message)
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-#    prompt = event.message.text
-#    llm_text = llm_responser(llm_server_url, prompt)
-    with ApiClient(configuration) as api_client: 
-        line_bot_api = MessagingApi(api_client)
-        print(event.message.text)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
-            )
-        )
+#    with ApiClient(configuration) as api_client: 
+#        line_bot_api = MessagingApi(api_client)
+#        print(event.message.text)
+#        line_bot_api.reply_message_with_http_info(
+#            ReplyMessageRequest(
+#                reply_token=event.reply_token,
+#                messages=[TextMessage(text=event.message.text)]
+#            )
+#        )
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 10000))
